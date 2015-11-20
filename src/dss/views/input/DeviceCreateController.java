95656d85761efbb7b91f6c656b29a2f88ee57a6a
Trixie <trixie_lirit@yahoo.com>
2015-11-20 12:51:20 +0100

    stuff to inputclient

--- src/dss/views/input/DeviceCreateController.java
+++ src/dss/views/input/DeviceCreateController.java
@@ -1,7 +1,221 @@
 package dss.views.input;
+import dss.models.device.Device;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
 
 /**
  * Created by jpp on 11/12/15.
  */
 public class DeviceCreateController {
+    @FXML
+    private CheckBox sound_jack35;
+
+    @FXML
+    private CheckBox colors_white;
+
+    @FXML
+    private CheckBox platform_ios;
+
+    @FXML
+    private CheckBox comms_usb;
+
+    @FXML
+    private TextField camera_secondary_mp;
+
+    @FXML
+    private CheckBox sensors_gyro;
+
+    @FXML
+    private CheckBox network_lte;
+
+    @FXML
+    private TextField display_size;
+
+    @FXML
+    private CheckBox colors_pink;
+
+    @FXML
+    private CheckBox display_multitouch;
+
+    @FXML
+    private TextField model;
+
+    @FXML
+    private TextField displaydimensions_height;
+
+    @FXML
+    private CheckBox colors_black;
+
+    @FXML
+    private CheckBox comms_bluetooth;
+
+    @FXML
+    private TextField camera_primary_width;
+
+    @FXML
+    private TextField memory_internalsize;
+
+    @FXML
+    private CheckBox memory_sd;
+
+    @FXML
+    private CheckBox simtype_nanosim;
+
+    @FXML
+    private CheckBox platform_windows;
+
+    @FXML
+    private CheckBox sound_loudspeaker;
+
+    @FXML
+    private CheckBox network_gsm;
+
+    @FXML
+    private CheckBox colors_orange;
+
+    @FXML
+    private CheckBox colors_gray;
+
+    @FXML
+    private TextField memory_ramsize;
+
+    @FXML
+    private CheckBox colors_red;
+
+    @FXML
+    private CheckBox sensors_compass;
+
+    @FXML
+    private CheckBox memory_microsd;
+
+    @FXML
+    private TextField display_ratio;
+
+    @FXML
+    private TextField displaydimensions_depth;
+
+    @FXML
+    private CheckBox comms_wlan;
+
+    @FXML
+    private TextField camera_primary_height;
+
+    @FXML
+    private TextField camera_primary_mp;
+
+    @FXML
+    private CheckBox colors_gold;
+
+    @FXML
+    private TextField displaydimensions_width;
+
+    @FXML
+    private CheckBox comms_ufc;
+
+    @FXML
+    private TextField display_protection;
+
+    @FXML
+    private CheckBox simtype_unknown;
+
+    @FXML
+    private CheckBox network_evdo;
+
+    @FXML
+    private CheckBox colors_blue;
+
+    @FXML
+    private CheckBox colors_green;
+
+    @FXML
+    private CheckBox sensors_magnetometer;
+
+    @FXML
+    private TextField displaydimensions_weight;
+
+    @FXML
+    private CheckBox sensors_proximity;
+
+    @FXML
+    private TextField display_width;
+
+    @FXML
+    private CheckBox memory_microsdhc;
+
+    @FXML
+    private CheckBox memory_mmcmobile;
+
+    @FXML
+    private TextField display_type;
+
+    @FXML
+    private CheckBox memory_mmc;
+
+    @FXML
+    private CheckBox sensors_barometer;
+
+    @FXML
+    private CheckBox platform_unknown;
+
+    @FXML
+    private TextField batterytime_talk;
+
+    @FXML
+    private CheckBox network_umts;
+
+    @FXML
+    private CheckBox simtype_fullsize;
+
+    @FXML
+    private CheckBox sensors_accelerometer;
+
+    @FXML
+    private CheckBox platform_android;
+
+    @FXML
+    private CheckBox network_cdma;
+
+    @FXML
+    private CheckBox comms_radio;
+
+    @FXML
+    private TextField display_density;
+
+    @FXML
+    private CheckBox comms_gps;
+
+    @FXML
+    private CheckBox colors_yellow;
+
+    @FXML
+    private CheckBox simtype_microsim;
+
+    @FXML
+    private CheckBox colors_silver;
+
+    @FXML
+    private TextField imageurl;
+
+    @FXML
+    private TextField display_height;
+
+    @FXML
+    private CheckBox network_hspa;
+
+    @FXML
+    private CheckBox simtype_minisim;
+
+    @FXML
+    private TextField batterytime_sleep;
+
+    @FXML
+    private TextField batterytime_music;
+
+public Device createDevice() {
+        Device device = new Device();
+        device.name 
+    }
 }
+
