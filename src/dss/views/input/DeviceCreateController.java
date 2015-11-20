c2a5490a4b92dca9741c6a7d589186c2e8f0a76d
Trixie <trixie_lirit@yahoo.com>
2015-11-20 15:17:04 +0100

    Input client not working yet.... please take a look at the radio buttons and the check boxes.. the main doesnt match with the input client..

--- src/dss/views/input/DeviceCreateController.java
+++ src/dss/views/input/DeviceCreateController.java
@@ -1,8 +1,9 @@
 package dss.views.input;
 import dss.models.device.Device;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 
 /**
@@ -16,7 +17,7 @@ public class DeviceCreateController {
     private CheckBox colors_white;
 
     @FXML
-    private CheckBox platform_ios;
+    private RadioButton platform_ios;
 
     @FXML
     private CheckBox comms_usb;
@@ -43,9 +44,6 @@ public class DeviceCreateController {
     private TextField model;
 
     @FXML
-    private TextField displaydimensions_height;
-
-    @FXML
     private CheckBox colors_black;
 
     @FXML
@@ -64,7 +62,7 @@ public class DeviceCreateController {
     private CheckBox simtype_nanosim;
 
     @FXML
-    private CheckBox platform_windows;
+    private RadioButton platform_windows;
 
     @FXML
     private CheckBox sound_loudspeaker;
@@ -94,10 +92,10 @@ public class DeviceCreateController {
     private TextField display_ratio;
 
     @FXML
-    private TextField displaydimensions_depth;
+    private CheckBox comms_wlan;
 
     @FXML
-    private CheckBox comms_wlan;
+    private TextField devicedimensions_weight;
 
     @FXML
     private TextField camera_primary_height;
@@ -109,10 +107,7 @@ public class DeviceCreateController {
     private CheckBox colors_gold;
 
     @FXML
-    private TextField displaydimensions_width;
-
-    @FXML
-    private CheckBox comms_ufc;
+    private CheckBox comms_nfc;
 
     @FXML
     private TextField display_protection;
@@ -130,10 +125,10 @@ public class DeviceCreateController {
     private CheckBox colors_green;
 
     @FXML
-    private CheckBox sensors_magnetometer;
+    private TextField devicedimensions_height;
 
     @FXML
-    private TextField displaydimensions_weight;
+    private CheckBox sensors_magnetometer;
 
     @FXML
     private CheckBox sensors_proximity;
@@ -154,10 +149,16 @@ public class DeviceCreateController {
     private CheckBox memory_mmc;
 
     @FXML
+    private TextField devicedimensions_width;
+
+    @FXML
+    private TextField devicedimensions_depth;
+
+    @FXML
     private CheckBox sensors_barometer;
 
     @FXML
-    private CheckBox platform_unknown;
+    private RadioButton platform_unknown;
 
     @FXML
     private TextField batterytime_talk;
@@ -172,7 +173,7 @@ public class DeviceCreateController {
     private CheckBox sensors_accelerometer;
 
     @FXML
-    private CheckBox platform_android;
+    private RadioButton platform_android;
 
     @FXML
     private CheckBox network_cdma;
@@ -213,9 +214,65 @@ public class DeviceCreateController {
     @FXML
     private TextField batterytime_music;
 
-public Device createDevice() {
+    public Device createDevice() {
         Device device = new Device();
-        device.name 
+        device.name = model.getText();
+        device.imageUrl = imageurl.getText();
+
+        Device.Body body = new Device.Body();
+        body.width = Double.parseDouble(devicedimensions_width.getText());
+        body.height = Double.parseDouble(devicedimensions_height.getText());
+        body.depth = Double.parseDouble(devicedimensions_depth.getText());
+        body.weight = Double.parseDouble(devicedimensions_weight.getText());
+        device.body = body;
+
+        Device.Battery battery =  new Device.Battery();
+        battery.sleep = Integer.parseInt(batterytime_sleep.getText());
+        battery.talk = Integer.parseInt(batterytime_talk.getText());
+        battery.music = Integer.parseInt(batterytime_music.getText());
+        device.battery = battery;
+
+
+        Device.Camera camera = new Device.Camera();
+        camera.primary.mp = Integer.parseInt(camera_primary_mp.getText());
+        camera.primary.width = Integer.parseInt(camera_primary_width.getText());
+        camera.primary.height = Integer.parseInt(camera_primary_height.getText());
+        camera.secondary.mp = Integer.parseInt(camera_secondary_mp.getText());
+        device.camera = camera;
+
+        Device.Com comms = new Device.Com();
+        comms.wlan = comms_wlan.selectedProperty().get();
+        comms.bluetooth = comms_bluetooth.selectedProperty().get();
+        comms.gps = comms_gps.selectedProperty().get();
+        comms.nfc = comms_nfc.selectedProperty().get();
+        comms.radio = comms_radio.selectedProperty().get();
+        comms.usb = comms_usb.selectedProperty().get();
+        device.com = comms;
+
+
+        final ToggleGroup group = new ToggleGroup();
+
+            platform_unknown.setToggleGroup(group);
+            platform_unknown.setSelected(true);
+            platform_android.setToggleGroup(group);
+            platform_android.setSelected(false);
+            platform_ios.setToggleGroup(group);
+            platform_ios.setSelected(false);
+            platform_windows.setToggleGroup(group);
+            platform_windows.setSelected(false);
+
+// I DON'T KNOW WHAT THIS DOES!
+        
+            group.selectedToggleProperty().addListener(new ChangeListener<Toggle>() {
+                @Override
+                public void changed(ObservableValue<? extends Toggle> ov, Toggle t, Toggle t1) {
+
+                    RadioButton chk = (RadioButton)t1.getToggleGroup().getSelectedToggle(); // Cast object to radio button
+
+                }
+            });
+        return device;
+
     }
 }
 
